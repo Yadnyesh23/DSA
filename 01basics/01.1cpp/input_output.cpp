0d253f63899994caa6b73d2fa8291cbc6eb4a9c3
Yadnyesh23 <yadnyeshhalde2006@gmail.com>
2026-06-08 11:44:34 +0530

    CPP input/output

---
+++ 01basics/01.1cpp/input_output.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<bits/stdc++.h> // This includes all the libraries from c++
+
+using namespace std;
+
+int main(){
+    int x, y;
+    cin >> x >> y; // Input 
+    cout << "Value of x : " << x << "\n" << "Value of y : " << y ; // Output
+    return 0;
+}
