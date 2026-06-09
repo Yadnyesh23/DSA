3b3ace7c949694f1e10b9a3dc7c5ee3bf2147761
Yadnyesh23 <yadnyeshhalde2006@gmail.com>
2026-06-09 20:28:57 +0530

    pattern - 13

---
+++ 01basics/01.3patterns/pattern13.cpp
@@ -0,0 +1,28 @@
+// Pattern
+// 1 
+// 2 3 
+// 4 5 6 
+// 7 8 9 10 
+// 11 12 13 14 15 
+
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int rows;
+    cout << "Enter no. of rows : ";
+    cin >> rows ;
+    int start = 1;
+    
+    for(int i = 1; i <= rows ; i++){
+        for(int j = 1; j <= i ; j++){
+            cout << start << " ";
+            start++;
+        }
+        cout << endl ;
+    }
+    
+    return 0;
+}
