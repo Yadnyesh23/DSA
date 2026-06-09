0aa99d9f94fdb1389160ea5576ca711072baa7ec
Yadnyesh23 <yadnyeshhalde2006@gmail.com>
2026-06-09 15:57:10 +0530

    pattern - 8

--- 01basics/01.3patterns/pattern8.cpp
+++ 01basics/01.3patterns/pattern8.cpp
@@ -18,13 +18,20 @@ int main(){
 
     for(int i = 0 ; i < rows ; i++){
         //Space
-        for(int j = 1 ; j <= cols - i -1 ; j++){
-            cout << " ";
+        for(int j = 0  ; j < i ; j++){
+            cout << "  ";
         }
         //Stars
-        for(int j = 0 ; j < (2 * i) + 1 ; j++ ){
-            cout << "*";
+        for(int j = 0 ; j < (2 * cols ) - ((2 * i )+ 1); j++ ){
+            cout << "* ";
+        }
+        
+        //Space
+        for(int j = 0  ; j < i ; j++){
+            cout << "  ";
         }
         cout << endl;
     }
     return 0;
+    
+}
