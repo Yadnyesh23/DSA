0bf3bdeb92df4d360c35bd21e2a83aa0d2fd0d96
Yadnyesh23 <yadnyeshhalde2006@gmail.com>
2026-06-10 16:39:27 +0530

    pattern - 17

---
+++ 01basics/01.3patterns/pattern17.cpp
@@ -0,0 +1,33 @@
+// Pattern 
+//         A 
+//       A B A 
+//     A B C B A 
+//   A B C D C B A 
+// A B C D E D C B A 
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows ;
+    cout << "Enter no. of rows : ";
+    cin >> rows ;
+    
+    
+    for(int i = 0 ; i < rows ; i++){
+        // space 
+        for(int j = 0 ; j < rows - i -1 ; j++){
+            cout << "  "  ;
+        }
+        char ch = 'A';
+        int breakpoint = ((2*i) + 1)/2;
+        
+        for(int j = 0 ; j < (2 * i) + 1 ; j++){
+            cout << ch << " ";
+            if(j < breakpoint) ch++ ;
+            else ch--;
+            
+        }
+        cout << endl ;
+    }
