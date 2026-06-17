4fabe632014d86fd163f79b660a00a210d3fd3b1
Yadnyesh23 <yadnyeshhalde2006@gmail.com>
2026-06-17 18:33:50 +0530

    Recursion - exercise 6

--- 01basics/01.6recursion/exercise6.cpp
+++ 01basics/01.6recursion/exercise6.cpp
@@ -1,22 +1,43 @@
 // Print sum of first n numbers
+#include <iostream>
+using namespace std;
+
+// Functional Way
+
+int summation(int n){
+    if(n == 0 || n == 1) return 1;
+    return n + summation(n-1);
+    
+}
+
+int main() {
+    int n ;
+    cout << "Enter the value of n : " ;
+    cin >> n ;
+   
+    int sum = summation(n);
+    cout << "Sum : " << sum ;
+    return 0;
+}
+
+// Parameterised way
 
 #include <iostream>
 using namespace std;
 
-int summation(int i, int n, int sum){
-    if(i > n) return sum;
-    sum += i;
-    i++;
-    
-    return summation(i,n,sum);
+void summation(int i, int sum){
+   if(i < 1) {
+       cout<<sum;
+       return ;
+   }
+   summation(i-1, sum+i);
 }
+
 int main() {
-    int n;
-    int i = 0;
-    int sum = 0;
-    cout << "Enter the value of n : ";
-    cin >> n;
-    sum = summation(i, n, sum);
-    cout << "Summation of fist "<< n << " numbers is " << sum ;
+    int n ;
+    cout << "Enter the value of n : " ;
+    cin >> n ;
+    int sum = 0 ; 
+   summation(n, sum);
     return 0;
 }
